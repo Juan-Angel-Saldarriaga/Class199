25061ad5303c2ef15bd77d814e1e1be66f843ea8
Juan Saldarriaga <juansaldarriaga@plataformapascal.com>
2024-11-01 18:22:39 -0500

    Class 199

---
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { MatLabel } from '@angular/material/form-field';
+
+@Component({
+  selector: 'app-layout-page',
+  templateUrl: './layout-page.component.html',
+  styles: ``
+})
+export class LayoutPageComponent {
+
+  public sidebarItems = [
+    {
+      label: 'Listado',
+      icon: 'label',
+      url: './list'
+    },
+    {
+      label: 'Añadir',
+      icon: 'add',
+      url: './new-hero'
+    },
+    {
+      label: 'Buscar',
+      icon: 'search',
+      url: './search'
+    },
+  ]
+
+}
